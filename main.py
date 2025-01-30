0abec133a9825ac67befdbdefaea601f73bd6c12
sharpcd <nikita.khala.cd2007@gmail.com>
2025-01-30 13:08:10 +0300

    v1.4 (new file scripts with more pharms)

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import os
 from schedule import start_schedule, stop_schedule,job  # Импортируем только нужные функции
+import sys
 
 # Запрос имени аптеки и ссылки
-cur_apteka = input("Введите название аптеки: ")
-cur_apteka_link = input("Введите ссылку на аптеку: ")
 
-def main():
+
+def main(cur_apteka,cur_apteka_link):
     # Запускаем планировщик в основном потоке
-
     start_schedule(cur_apteka, cur_apteka_link)
 
+
 if __name__ == "__main__":
-    main()
+    # Проверяем, что передано 2 аргумента
+    if len(sys.argv) != 3:
+        print("Ошибка: требуется 2 аргумента (аптека и ссылка).")
+        sys.exit(1)
+
+    # Получаем аргументы из командной строки
+    cur_apteka = sys.argv[1]
+    cur_apteka_link = sys.argv[2]
+
+    # Вызываем основную функцию
+    main(cur_apteka, cur_apteka_link)
