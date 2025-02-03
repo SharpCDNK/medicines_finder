e91f11d5f14b52742a0a5c754f35038c74173b45
sharpcd <nikita.khala.cd2007@gmail.com>
2025-02-03 20:54:46 +0300

    v1.8.4 powershell in script_parser

--- script_parser.py
+++ script_parser.py
@@ -2,13 +2,15 @@ import subprocess
 import sys
 import platform
 
-
 # Функция для чтения данных из файла
 def read_data(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-    return [line.strip().split() for line in lines if line.strip()]
-
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        return [line.strip().split() for line in lines if line.strip()]
+    except FileNotFoundError:
+        print(f'Ошибка: файл {file_path} не найден.')
+        sys.exit(1)
 
 # Путь к файлу с данными
 file_path = 'data.txt'  # Укажите путь к вашему файлу
@@ -28,9 +30,9 @@ for entry in data:
             command = f'powershell -NoExit -Command "python main.py \'{name}\' \'{url}\'; Write-Host \\"Нажмите любую клавишу для выхода...\\"; $Host.UI.RawUI.ReadKey() | Out-Null"'
             subprocess.Popen(command, shell=True)
         else:
-            # Для Linux/macOS
+            # Для Linux
             subprocess.Popen(['gnome-terminal', '--', 'python', 'main.py', name, url])  # Для Linux
-            # или
-            # subprocess.Popen(['osascript', '-e', f'tell application "Terminal" to do script "python main.py {name} {url}"'])  # Для macOS
+            # Для macOS
+            # subprocess.Popen(['osascript', '-e', f'tell application "Terminal" to do script "python main.py {name} {url}"'])
     else:
         print(f'Пропущена строка: {entry} - неверный формат')
